313e8f06fe0ac93ae100dfdae1105a31be2d164f
Corey Tattam <corey_tattam@hotmail.com>
2019-05-26 23:04:37 +1000

    Add the SeoReportsController and register classes with the D.I. engine.

---
+++ SeoAnalysis.WebUI/Controllers/SeoReportsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeoAnalysis.Core.Models;
+using SeoAnalysis.Core.Services;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SeoAnalysis.WebUI.Controllers
+{
+    [Route("api/[controller]")]
+    public class SeoReportsController : BaseController
+    {
+
+        #region " - - - - - - Fields - - - - - - "
+
+        private readonly ISeoAnalysisReportService m_SeoReportService;
+
+        #endregion //Fields
+
+        #region " - - - - - - Constructors - - - - - - "
+
+        public SeoReportsController(ISeoAnalysisReportService seoReportService)
+        {
+            this.m_SeoReportService = seoReportService ?? throw new System.ArgumentNullException(nameof(seoReportService));
+        }
+
+        #endregion //Constructors
+
+        #region " - - - - - - Actions - - - - - - "
+
+        [HttpGet]
+        [ProducesResponseType(typeof(SeoAnalysisReport), (int)HttpStatusCode.OK)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<SeoAnalysisReport>> GetSeoAnalysisReport(string keywords, string url) =>
+            await this.m_SeoReportService.GetSeoAnalysisReport(keywords, url);
+
+        #endregion //Actions
+
+    } //SeoReportsController
+}
