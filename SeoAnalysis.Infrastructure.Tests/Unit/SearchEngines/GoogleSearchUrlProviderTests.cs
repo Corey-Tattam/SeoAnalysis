e0bff019c3934452b84ea2aefe3e23d4110a87c7
Corey Tattam <corey_tattam@hotmail.com>
2019-05-26 18:11:47 +1000

    Add Google Search Engine concrete classes and tests.

---
+++ SeoAnalysis.Infrastructure.Tests/Unit/SearchEngines/GoogleSearchUrlProviderTests.cs
@@ -0,0 +1,101 @@
+﻿using SeoAnalysis.Infrastructure.SearchEngines;
+using System;
+using Xunit;
+
+namespace SeoAnalysis.Infrastructure.Tests.Unit.SearchEngines
+{
+    public class GoogleSearchUrlProviderTests
+    {
+
+        #region " - - - - - - CreateSearchUrl Tests - - - - - - "
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        public void CreateSearchUrl_InvalidNumberOfResultsArgument_ThrowsArgumentException(int numberOfResults)
+        {
+            // Arrange
+            var _SearchUrlProvider = new GoogleSearchUrlProvider();
+
+            // Act
+            var _Exception = Record.Exception(() => _SearchUrlProvider.CreateSearchUrl("keywords", numberOfResults));
+
+            // Assert
+            Assert.NotNull(_Exception);
+            Assert.IsType<ArgumentException>(_Exception);
+        } //CreateSearchUrl_InvalidNumberOfResultsArgument_ThrowsArgumentException
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void CreateSearchUrl_InvalidKeywordsArgument_ThrowsArgumentException(string keywords)
+        {
+            // Arrange
+            var _SearchUrlProvider = new GoogleSearchUrlProvider();
+
+            // Act
+            var _Exception = Record.Exception(() => _SearchUrlProvider.CreateSearchUrl(keywords, 1));
+
+            // Assert
+            Assert.NotNull(_Exception);
+            Assert.IsType<ArgumentException>(_Exception);
+        } //CreateSearchUrl_InvalidKeywordsArgument_ThrowsArgumentException
+
+        [Theory]
+        [InlineData("  LeadingWhitespace",          "LeadingWhitespace")]
+        [InlineData("TrailingWhitespace  ",         "TrailingWhitespace")]
+        [InlineData("White Space",                  "White+Space")]
+        [InlineData("Running    Whitespace",        "Running+Whitespace")]
+        [InlineData("  Lots   of    whitespace  ",  "Lots+of+whitespace")]
+        public void CreateSearchUrl_KeywordStringWithRunningSpaces_MatchExpectedQueryStringValue(string keywords, string expectedQueryStringValue)
+        {
+            // Arrange
+            var _SearchUrlProvider = new GoogleSearchUrlProvider();
+
+            // Act
+            var _SearchUrl = _SearchUrlProvider.CreateSearchUrl(keywords, 10);
+
+            // Assert
+            Assert.Contains(expectedQueryStringValue, _SearchUrl.Query);
+        } //CreateSearchUrl_KeywordStringWithRunningSpaces_MatchExpectedQueryStringValue
+
+        [Theory]
+        [InlineData("plus+",    "plus%2b")]
+        [InlineData("plus +",   "plus+%2b")]
+        public void CreateSearchUrl_KeywordStringWithSpecialCharacters_MatchExpectedQueryStringValue(string keywords, string expectedQueryStringValue)
+        {
+            // Arrange
+            var _SearchUrlProvider = new GoogleSearchUrlProvider();
+
+            // Act
+            var _SearchUrl = _SearchUrlProvider.CreateSearchUrl(keywords, 10);
+
+            // Assert
+            Assert.Contains(expectedQueryStringValue, _SearchUrl.Query);
+        } //CreateSearchUrl_KeywordStringWithSpecialCharacters_MatchExpectedQueryStringValue
+
+        [Fact]
+        public void CreateSearchUrl_ValidInput_SuccessfulCreation()
+        {
+            // Arrange
+            var _SearchUrlProvider = new GoogleSearchUrlProvider();
+            var _Keywords = "key words";
+            var _NumberOfResults = 10;
+
+            var _BaseUrl = GoogleSearchUrlProvider.GOOGLE_SEARCH_URL_BASE;
+            var _SearchParameterName = GoogleSearchUrlProvider.SEARCH_QUERY_PARAMETER_NAME;
+            var _MaxResultsParameterName = GoogleSearchUrlProvider.MAX_RESULTS_QUERY_PARAMETER_NAME;
+            var _ExpectedResult = new Uri($"{_BaseUrl}?{_SearchParameterName}=key+words&{_MaxResultsParameterName}={_NumberOfResults}");
+
+            // Act
+            var _SearchUrl = _SearchUrlProvider.CreateSearchUrl(_Keywords, _NumberOfResults);
+
+            // Assert
+            Assert.Equal(_SearchUrl, _ExpectedResult);
+        } //CreateSearchUrl_ValidInput_SuccessfulCreation
+
+        #endregion //CreateSearchUrl Tests
+
+    } //GoogleSearchUrlProviderTests
+}
---
+++ SeoAnalysis.Infrastructure/SearchEngines/GoogleResultsPageAnalyser.cs
@@ -0,0 +1,68 @@
+﻿using SeoAnalysis.Core.Net;
+using SeoAnalysis.Core.SearchEngines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeoAnalysis.Infrastructure.SearchEngines
+{
+    public class GoogleResultsPageAnalyser : ISearchEngineResultsPageAnalyser
+    {
+
+        #region " - - - - - - Fields - - - - - - "
+
+        private const int NUM_SEARCH_RESULTS = 100;
+
+        private readonly IHttpRequestSender m_HttpRequestSender;
+        private readonly ISearchEngineResultsPageParser<GoogleResultsPageAnalyser> m_SearchResultParser;
+        private readonly ISearchUrlProvider<GoogleResultsPageAnalyser> m_SearchUrlProvider;
+
+        #endregion //Fields
+
+        #region " - - - - - - Constructors - - - - - - "
+
+        public GoogleResultsPageAnalyser
+        (
+            IHttpRequestSender httpRequestSender, 
+            ISearchEngineResultsPageParser<GoogleResultsPageAnalyser> searchResultParser,
+            ISearchUrlProvider<GoogleResultsPageAnalyser> searchUrlProvider
+        )
+        {
+            this.m_HttpRequestSender    = httpRequestSender  ?? throw new ArgumentNullException(nameof(httpRequestSender));
+            this.m_SearchResultParser   = searchResultParser ?? throw new ArgumentNullException(nameof(searchResultParser));
+            this.m_SearchUrlProvider    = searchUrlProvider  ?? throw new ArgumentNullException(nameof(searchUrlProvider));
+        }
+
+        #endregion //Constructors
+
+        #region " - - - - - - ISearchEngineResultsPageAnalyser Implementation - - - - - - "
+
+        /// <summary>
+        /// Get the positions of a domain in a Search Engine Results Page from a keyword search.
+        /// </summary>
+        /// <param name="keywords">The keywords to search.</param>
+        /// <param name="domain">The domain to match.</param>
+        /// <returns>The position numbers of all matches in the results.</returns>
+        public async Task<IEnumerable<int>> GetSearchEngineResultsPagePositions(string keywords, string domain)
+        {
+            if (string.IsNullOrWhiteSpace(keywords))    throw new ArgumentException("At least one keyword must be specified.", nameof(keywords));
+            if (string.IsNullOrWhiteSpace(domain))      throw new ArgumentException("The domain must be specified.", nameof(domain));
+
+            // Create the Search URL.
+            var _SearchUrl = this.m_SearchUrlProvider.CreateSearchUrl(keywords, NUM_SEARCH_RESULTS);
+
+            // Send the Request.
+            var _SearchHtmlContent = await this.m_HttpRequestSender.GetStringContentAsync(_SearchUrl);
+
+            // Parse HTML Content as Search Results.
+            var _SearchResults = this.m_SearchResultParser.ParseHtmlSearchEngineResults(_SearchHtmlContent);
+
+            // Extract relevant result positions and return.
+            return _SearchResults.Where(s => s.ResultLink.Contains(domain)).Select(s => s.Position);
+        } //GetSearchEngineResultsPagePositions
+
+        #endregion //ISearchEngineResultsPageAnalyser Implementation
+
+    } //GoogleResultsPageAnalyser
+}
