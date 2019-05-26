39b753f95ae11c18fa690dabc746ef980275f150
Corey Tattam <corey_tattam@hotmail.com>
2019-05-26 22:59:27 +1000

    Add CustomExceptionFilter and a Base API Controller
    Additionally, add references to the Core and Infrastructure projects
    in the WebUI project.

---
+++ SeoAnalysis.WebUI/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeoAnalysis.WebUI.Infrastructure.Filters;
+
+namespace SeoAnalysis.WebUI.Controllers
+{
+    [ApiController]
+    [CustomExceptionFilter]
+    public abstract class BaseController : ControllerBase
+    {
+    } //BaseController
+}
