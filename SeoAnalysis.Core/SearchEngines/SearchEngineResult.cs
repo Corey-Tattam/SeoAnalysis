6fa251cda8bacf5e9522a2d320c950bf181d5a80
Corey Tattam <corey_tattam@hotmail.com>
2019-05-26 17:56:19 +1000

    Add SearchEngine interfaces and models to the core project

---
+++ SeoAnalysis.Core/SearchEngines/SearchEngineResult.cs
@@ -0,0 +1,15 @@
+﻿namespace SeoAnalysis.Core.SearchEngines
+{
+    public class SearchEngineResult
+    {
+
+        #region " - - - - - - Properties - - - - - - "
+
+        public int Position { get; set; }
+
+        public string ResultLink { get; set; }
+
+        #endregion //Properties
+
+    } //SearchEngineResult
+}
